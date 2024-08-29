7eaaadc26fb5a9d458b8018cf68dd8f026d5043c
rafittu <rafittu@outlook.com>
2024-08-29 18:25:06 -0300

    feat: function to validate user credentials

--- src/modules/auth/services/signin.service.ts
+++ src/modules/auth/services/signin.service.ts
@@ -1,12 +1,47 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../../prisma.service';
+import { SecurityService } from '../../../common/services/security.service';
+
 import { CredentialsDto } from '../dto/credentials.dto';
+import { AppError } from '../../../common/errors/Error';
 
 @Injectable()
 export class SignInService {
-  constructor() {}
+  constructor(
+    private securityService: SecurityService,
+    private prisma: PrismaService,
+  ) {}
 
   async validateUser(credentials: CredentialsDto) {
+    const { email, password } = credentials;
+
     try {
-    } catch (error) {}
+      const user = await this.prisma.admin.findFirst({
+        where: { email },
+      });
+
+      if (user) {
+        const isPasswordValid = await this.securityService.comparePasswords(
+          password,
+          user.password,
+        );
+
+        if (isPasswordValid) {
+          return {
+            id: user.id,
+            name: user.name,
+            email,
+          };
+        }
+      }
+
+      throw new AppError(
+        'signin-service.validateUser',
+        401,
+        'email or password is invalid',
+      );
+    } catch (error) {
+      throw error;
+    }
   }
 }
