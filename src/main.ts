49353474abdd4429fcc6500f0df9a7f04afe68bf
rafittu <rafittu@outlook.com>
2024-08-29 09:15:48 -0300

    chore: protect app with helmet middleware

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+  app.use(helmet.hidePoweredBy());
+  app.use(helmet.contentSecurityPolicy());
+
   await app.listen(3000);
 }
 bootstrap();
