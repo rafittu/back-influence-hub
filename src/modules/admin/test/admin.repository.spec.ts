4f2e354e6d8ccc7e8230ee0b0c9ad15ba3e7c874
rafittu <rafittu@outlook.com>
2024-08-29 15:10:15 -0300

    test: create admin user successfully

--- src/modules/admin/test/admin.repository.spec.ts
+++ src/modules/admin/test/admin.repository.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../../../prisma.service';
 import { AdminRepository } from '../repository/admin.repository';
+import { MockAdmin, MockCreateAdmin } from './mocks/admin.mock';
 
 describe('AdminRepository', () => {
   let adminRepository: AdminRepository;
@@ -18,5 +19,18 @@ describe('AdminRepository', () => {
   it('should be defined', () => {
     expect(adminRepository).toBeDefined();
     expect(prismaService).toBeDefined();
+  });
+
+  describe('create user', () => {
+    it('should create a new user successfully', async () => {
+      jest
+        .spyOn(prismaService.admin, 'create')
+        .mockResolvedValueOnce(MockAdmin);
+
+      const result = await adminRepository.createAdmin(MockCreateAdmin);
+
+      expect(prismaService.admin.create).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(MockAdmin);
+    });
   });
 });
