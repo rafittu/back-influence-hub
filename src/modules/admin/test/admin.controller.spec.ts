9af80bde4ce094f0d6d5664f0cf3482048a7ebfb
rafittu <rafittu@outlook.com>
2024-08-29 14:17:10 -0300

    test: setup controller tests

--- src/modules/admin/test/admin.controller.spec.ts
+++ src/modules/admin/test/admin.controller.spec.ts
@@ -1,17 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AdminController } from './admin.controller';
-import { AdminService } from './admin.service';
+import { AdminController } from '../admin.controller';
+import { CreateAdminService } from '../services/create-admin.service';
 
 describe('AdminController', () => {
   let controller: AdminController;
+  let createAdmin: CreateAdminService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AdminController],
-      providers: [AdminService],
+      providers: [
+        {
+          provide: CreateAdminService,
+          useValue: {
+            execute: jest.fn().mockResolvedValue(''),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<AdminController>(AdminController);
+    createAdmin = module.get<CreateAdminService>(CreateAdminService);
   });
 
   it('should be defined', () => {
