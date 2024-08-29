0e27ef2ef56c8aa23739c806c1d4a69b4cb4a5ae
rafittu <rafittu@outlook.com>
2024-08-29 15:11:34 -0300

    test: throw error if email is already in use

--- src/modules/admin/test/admin.repository.spec.ts
+++ src/modules/admin/test/admin.repository.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../../../prisma.service';
 import { AdminRepository } from '../repository/admin.repository';
 import { MockAdmin, MockCreateAdmin } from './mocks/admin.mock';
+import { AppError } from '../../../common/errors/Error';
 
 describe('AdminRepository', () => {
   let adminRepository: AdminRepository;
@@ -31,6 +32,21 @@ describe('AdminRepository', () => {
 
       expect(prismaService.admin.create).toHaveBeenCalledTimes(1);
       expect(result).toEqual(MockAdmin);
+    });
+
+    it('should throw an error if email is already in use', async () => {
+      jest.spyOn(prismaService.admin, 'create').mockRejectedValueOnce({
+        code: 'P2002',
+        meta: { target: ['email'] },
+      });
+
+      try {
+        await adminRepository.createAdmin(MockCreateAdmin);
+      } catch (error) {
+        expect(error).toBeInstanceOf(AppError);
+        expect(error.code).toBe(409);
+        expect(error.message).toBe('email already in use');
+      }
     });
   });
 });
