f26a634ccff844596dcc2308a3b0fe882605af1b
rafittu <rafittu@outlook.com>
2024-08-29 18:47:50 -0300

    chore: update auth module providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,9 +4,16 @@ import { LocalStrategy } from './infra/strategies/local.strategy';
 import { SignInService } from './services/signin.service';
 import { PrismaService } from '../../prisma.service';
 import { SecurityService } from '../../common/services/security.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
-  providers: [PrismaService, SecurityService, LocalStrategy, SignInService],
+  providers: [
+    PrismaService,
+    SecurityService,
+    JwtService,
+    LocalStrategy,
+    SignInService,
+  ],
 })
 export class AuthModule {}
