84109902890dbb396d6ef44bb884315effef5b33
rafittu <rafittu@outlook.com>
2024-08-29 18:42:46 -0300

    feat: signin controller function

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,11 +1,26 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Request,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
+import { AppError } from '../../common/errors/Error';
+import { HttpExceptionFilter } from '../../common/filter/http-exception.filter';
+import { LocalAuthGuard } from './infra/guards/local-auth.guard';
+import { IAuthRequest } from './interfaces/service.interface';
+import { SignInService } from './services/signin.service';
 
-@Controller('auth')
+@UseFilters(new HttpExceptionFilter(new AppError()))
+@Controller()
 export class AuthController {
-  constructor() {}
+  constructor(private readonly signInService: SignInService) {}
 
-  @Post()
-  create() {
-    return 'this.authService.create(createAuthDto)';
+  @Post('/signin')
+  @UseGuards(LocalAuthGuard)
+  async signin(@Request() req: IAuthRequest) {
+    const { user } = req;
+
+    return await this.signInService.execute(user);
   }
 }
