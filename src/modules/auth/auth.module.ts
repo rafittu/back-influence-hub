15c09959f4d43693718bda2e5324b8c235fe9f2d
rafittu <rafittu@outlook.com>
2024-08-29 18:06:52 -0300

    chore: update auth module providers

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { LocalStrategy } from './infra/strategies/local.strategy';
+import { SignInService } from './services/signin.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [],
+  providers: [LocalStrategy, SignInService],
 })
 export class AuthModule {}
