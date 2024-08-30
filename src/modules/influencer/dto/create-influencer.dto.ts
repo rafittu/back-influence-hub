b821aab02193fbd727f62d2a5394a8bae95283b5
rafittu <rafittu@outlook.com>
2024-08-30 11:10:18 -0300

    feat: create influencer dto

--- src/modules/influencer/dto/create-influencer.dto.ts
+++ src/modules/influencer/dto/create-influencer.dto.ts
@@ -1,1 +1,44 @@
-export class CreateInfluencerDto {}
+import {
+  IsArray,
+  IsEnum,
+  ArrayNotEmpty,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+import { Niche } from '../enums/niche.enum';
+
+export class CreateInfluencerDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  reach: number;
+
+  @IsString()
+  photo?: string;
+
+  @IsArray()
+  @ArrayNotEmpty()
+  @IsEnum(Niche, {
+    each: true,
+    message: 'Each niche must be one of the predefined values',
+  })
+  niches: Niche[];
+
+  @IsNotEmpty()
+  @IsString()
+  zipCode: string;
+
+  @IsNotEmpty()
+  @IsString()
+  street: string;
+
+  @IsNotEmpty()
+  @IsString()
+  number: string;
+}
