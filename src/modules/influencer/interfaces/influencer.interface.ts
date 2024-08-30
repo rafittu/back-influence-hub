8ed9cc99242a59dfd6b39165509ee4f400f8d902
rafittu <rafittu@outlook.com>
2024-08-30 13:34:24 -0300

    feat: create influencer interface

---
+++ src/modules/influencer/interfaces/influencer.interface.ts
@@ -0,0 +1,12 @@
+export interface ICreateInfluencer {
+  name: string;
+  username: string;
+  reach: number;
+  photo?: string;
+  niches: string[];
+  zipCode: string;
+  state: string;
+  city: string;
+  street: string;
+  number: string;
+}
