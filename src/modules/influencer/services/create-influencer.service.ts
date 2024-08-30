6a60bb0c7a276a7ea89a69f67cb105bf3beff010
rafittu <rafittu@outlook.com>
2024-08-30 12:06:38 -0300

    feat: get influencer address

--- src/modules/influencer/services/create-influencer.service.ts
+++ src/modules/influencer/services/create-influencer.service.ts
@@ -1,16 +1,14 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 import { AppError } from '../../../common/errors/Error';
-import { InfluencerRepository } from '../repository/influencer.repository';
-import { IInfluencerRepository } from '../interfaces/repository.interface';
-import { Influencer } from '@prisma/client';
+// import { InfluencerRepository } from '../repository/influencer.repository';
+// import { IInfluencerRepository } from '../interfaces/repository.interface';
+// import { Influencer } from '@prisma/client';
 import { CreateInfluencerDto } from '../dto/create-influencer.dto';
 
 @Injectable()
 export class CreateInfluencerService {
-  constructor(
-    @Inject(InfluencerRepository)
-    private readonly influencerRepository: IInfluencerRepository<Influencer>,
-  ) {}
+  constructor() {} // private readonly influencerRepository: IInfluencerRepository<Influencer>, // @Inject(InfluencerRepository)
 
   private transformTimestamps<T extends { created_at: Date; updated_at: Date }>(
     entity: T,
@@ -26,9 +24,55 @@ export class CreateInfluencerService {
     };
   }
 
-  async execute(data: CreateInfluencerDto) {
+  private async getAddress(zipCode: string) {
     try {
-      console.log(data);
+      const response = await axios.get(
+        `https://viacep.com.br/ws/${zipCode}/json/`,
+      );
+
+      if (response.data.erro) {
+        throw new Error();
+      }
+
+      return response.data;
+    } catch (error) {
+      throw new AppError(
+        'create-influencer.getAddress',
+        400,
+        'error fetching address from ViaCEP',
+      );
+    }
+  }
+
+  async execute(data: CreateInfluencerDto) {
+    const zipCodeRegex = /^[0-9]{8}$/;
+
+    try {
+      const formatedZipCode = data.zipCode.replace('-', '');
+
+      const isZipCodeValid = zipCodeRegex.test(formatedZipCode);
+      if (!isZipCodeValid) {
+        throw new AppError(
+          'influencer-service.createInfluencer',
+          400,
+          'invalid zipcode format',
+        );
+      }
+
+      const address = await this.getAddress(formatedZipCode);
+      const { logradouro, localidade, uf } = address;
+
+      const influencerData = {
+        ...data,
+        zipCode: formatedZipCode,
+        street: logradouro,
+        city: localidade,
+        state: uf,
+      };
+
+      console.log(influencerData);
+
+      return data;
     } catch (error) {
       if (error instanceof AppError) {
         throw error;
