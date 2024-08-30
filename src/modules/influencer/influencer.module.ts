b51955f50c1ef48c59bbdeabff2d22488ffb7e1d
rafittu <rafittu@outlook.com>
2024-08-30 16:21:04 -0300

    feat: create influencer repository function

--- src/modules/influencer/influencer.module.ts
+++ src/modules/influencer/influencer.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InfluencerController } from './influencer.controller';
 import { CreateInfluencerService } from './services/create-influencer.service';
+import { InfluencerRepository } from './repository/influencer.repository';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [InfluencerController],
-  providers: [CreateInfluencerService],
+  providers: [PrismaService, InfluencerRepository, CreateInfluencerService],
 })
 export class InfluencerModule {}
---
+++ src/modules/influencer/interfaces/repository.interface.ts
@@ -0,0 +1,5 @@
+import { ICreateInfluencer } from './influencer.interface';
+
+export interface IInfluencerRepository<Influencer> {
+  createInfluencer(data: ICreateInfluencer): Promise<Influencer>;
+}
