32fc421b79e5d48f4794783bcf0ad67d83e58dd5
rafittu <rafittu@outlook.com>
2024-08-31 10:43:14 -0300

    feat: update influencer dto

--- src/modules/influencer/dto/update-influencer.dto.ts
+++ src/modules/influencer/dto/update-influencer.dto.ts
@@ -1,4 +1,41 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateInfluencerDto } from './create-influencer.dto';
+import { IsArray, IsEnum, IsOptional, IsString } from 'class-validator';
+import { Niche } from '../enums/niche.enum';
 
-export class UpdateInfluencerDto extends PartialType(CreateInfluencerDto) {}
+export class UpdateInfluencerDto extends PartialType(CreateInfluencerDto) {
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  username: string;
+
+  @IsOptional()
+  reach: number;
+
+  @IsOptional()
+  @IsString()
+  photo?: string;
+
+  @IsOptional()
+  @IsArray()
+  @IsEnum(Niche, {
+    each: true,
+    message: 'Each niche must be one of the predefined values',
+  })
+  niches: Niche[];
+
+  @IsOptional()
+  @IsString()
+  zipCode: string;
+
+  @IsOptional()
+  @IsString()
+  street: string;
+
+  @IsOptional()
+  @IsString()
+  number: string;
+}
