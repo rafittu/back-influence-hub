01af1fd316f63bf0f774e84ef4d0e3ee4e708ef8
rafittu <rafittu@outlook.com>
2024-08-31 12:24:39 -0300

    chore: update brand providers

--- src/modules/brand/brand.module.ts
+++ src/modules/brand/brand.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BrandController } from './brand.controller';
 import { PrismaService } from 'src/prisma.service';
 import { CreateBrandService } from './services/create-brand.service';
+import { BrandRepository } from './repository/brand.repository';
 
 @Module({
   controllers: [BrandController],
-  providers: [PrismaService, CreateBrandService],
+  providers: [PrismaService, BrandRepository, CreateBrandService],
 })
 export class BrandModule {}
