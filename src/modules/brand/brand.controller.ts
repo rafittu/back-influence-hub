af1a0f3cbc6b9becd5475c296bb243152bc6cd4c
rafittu <rafittu@outlook.com>
2024-08-31 12:06:58 -0300

    feat: create brand controller function

--- src/modules/brand/brand.controller.ts
+++ src/modules/brand/brand.controller.ts
@@ -5,18 +5,21 @@ import {
   Patch,
   Delete,
   UseFilters,
+  Body,
 } from '@nestjs/common';
 import { AppError } from 'src/common/errors/Error';
 import { HttpExceptionFilter } from 'src/common/filter/http-exception.filter';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { CreateBrandService } from './services/create-brand.service';
 
 @UseFilters(new HttpExceptionFilter(new AppError()))
 @Controller('brand')
 export class BrandController {
-  constructor() {}
+  constructor(private readonly createBrand: CreateBrandService) {}
 
-  @Post()
-  create() {
-    return 'this.brandService.create(createBrandDto)';
+  @Post('/create')
+  async create(@Body() body: CreateBrandDto) {
+    return await this.createBrand.execute(body);
   }
 
   @Get()
