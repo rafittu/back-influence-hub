87bd317896b7314148b0f604574ce394c6eb5a36
rafittu <rafittu@outlook.com>
2024-09-01 16:52:59 -0300

    feat: endpoint to get user by jwt

--- src/modules/auth/infra/decorators/current-user.decorator.ts
+++ src/modules/auth/infra/decorators/current-user.decorator.ts
@@ -1,8 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { IAuthRequest, IUserFromJwt } from '../../interfaces/service.interface';
+import { IAuthRequest, IUserPayload } from '../../interfaces/service.interface';
 
 export const CurrentUser = createParamDecorator(
-  (_data: unknown, context: ExecutionContext): IUserFromJwt => {
+  (_data: unknown, context: ExecutionContext): IUserPayload => {
     const request = context.switchToHttp().getRequest<IAuthRequest>();
 
     return request.user;
