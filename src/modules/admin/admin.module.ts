4dd50649c268798a141c77e769963dad2fa5db70
rafittu <rafittu@outlook.com>
2024-09-11 15:36:46 -0300

    chore: update admin module provider

--- src/modules/admin/admin.module.ts
+++ src/modules/admin/admin.module.ts
@@ -5,6 +5,7 @@ import { AdminRepository } from './repository/admin.repository';
 import { SecurityService } from '../../common/services/security.service';
 import { CreateAdminService } from './services/create-admin.service';
 import { FindAllAdminsService } from './services/all-admins.service';
+import { FindOneAdminService } from './services/find-one-admin.service';
 
 @Module({
   controllers: [AdminController],
@@ -14,6 +15,7 @@ import { FindAllAdminsService } from './services/all-admins.service';
     SecurityService,
     CreateAdminService,
     FindAllAdminsService,
+    FindOneAdminService,
   ],
 })
 export class AdminModule {}
