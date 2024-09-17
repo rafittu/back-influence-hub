d9b723d4bbe6610ec7b7bad7ff69afac04d258c4
rafittu <rafittu@outlook.com>
2024-09-17 15:12:44 -0300

    test: validate user credentials successfully

--- src/modules/auth/test/auth.service.spec.ts
+++ src/modules/auth/test/auth.service.spec.ts
@@ -3,13 +3,18 @@ import { SignInService } from '../services/signin.service';
 import { JwtService } from '@nestjs/jwt';
 import { SecurityService } from '../../../common/services/security.service';
 import { PrismaService } from '../../../prisma.service';
+import {
+  MockUserCredentials,
+  MockUserData,
+  MockUserPayload,
+} from './mocks/auth.mock';
 
 describe('AuthService', () => {
   let signInService: SignInService;
 
   let jwtService: JwtService;
   let securityService: SecurityService;
-  let prisma: PrismaService;
+  let prismaService: PrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -20,13 +25,30 @@ describe('AuthService', () => {
 
     jwtService = module.get<JwtService>(JwtService);
     securityService = module.get<SecurityService>(SecurityService);
-    prisma = module.get<PrismaService>(PrismaService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(signInService).toBeDefined();
     expect(jwtService).toBeDefined();
     expect(securityService).toBeDefined();
-    expect(prisma).toBeDefined();
+    expect(prismaService).toBeDefined();
+  });
+
+  describe('signin', () => {
+    it('should validate user credentials successfully', async () => {
+      jest
+        .spyOn(prismaService.admin, 'findFirst')
+        .mockResolvedValueOnce(MockUserData);
+
+      jest
+        .spyOn(securityService, 'comparePasswords')
+        .mockResolvedValue(true as never);
+
+      const result = await signInService.validateUser(MockUserCredentials);
+
+      expect(prismaService.admin.findFirst).toHaveBeenCalledTimes(1);
+      expect(result).toEqual(MockUserPayload);
+    });
   });
 });
--- src/modules/auth/test/mocks/auth.mock.ts
+++ src/modules/auth/test/mocks/auth.mock.ts
@@ -5,16 +5,26 @@ import {
   IUserToken,
 } from '../../interfaces/service.interface';
 import { CredentialsDto } from '../../dto/credentials.dto';
+import { Admin } from '@prisma/client';
 
 export const MockUserCredentials: CredentialsDto = {
   email: faker.internet.email(),
   password: faker.internet.password(),
 };
 
-export const MockUserPayload: IUserPayload = {
+export const MockUserData: Admin = {
   id: faker.number.int(),
   name: faker.person.fullName(),
   email: MockUserCredentials.email,
+  password: MockUserCredentials.password,
+  created_at: new Date(),
+  updated_at: new Date(),
+};
+
+export const MockUserPayload: IUserPayload = {
+  id: MockUserData.id,
+  name: MockUserData.name,
+  email: MockUserData.email,
 };
 
 export const MockAuthRequest: IAuthRequest = {
